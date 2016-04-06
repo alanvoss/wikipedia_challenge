dbe9b04ed3c3313da07808def8ecb89db94800a1
Alan Voss <avoss@rent.com>
2016-04-05 20:23:08 -0700

    our initial attempt at crawling wikipedia and finding cycles

---
+++ lib/crawler.rb
@@ -0,0 +1,48 @@
+require 'curb'
+require 'set'
+require 'pry'
+
+class WikipediaCrawler
+
+  def initialize(start_page, options = {})
+    @start_page = start_page
+    @options = options
+    @seen_pages = Set.new
+  end
+
+  def execute(url = @start_page)
+    name = ""
+    while @seen_pages.add?(url) || is_philospher?(name)
+      puts "URL: #{url}, NAME: #{name}"
+      begin
+        content = get_page(url)
+        url, name = find_link(content)
+      rescue
+        return "cycle"
+      end
+    end
+
+    is_philospher?(name) ? "philosopher" : "cycle"
+  end
+
+  private
+
+  def is_philospher?(name)
+    name.downcase.include?("philosphopher")
+  end
+
+  def get_page(url)
+    Curl.get(url).body
+  end
+
+  def find_link(html)
+    link = html.match(/<a href *= *"([^"]+)"[^>]*>([^<]+)/)
+    url = link[1]
+    url.sub!(%r{^(?=/)}, "https://en.wikipedia.org")
+    name = link[2]
+    [url, name]
+  end
+end
+
+
+puts WikipediaCrawler.new("https://en.wikipedia.org/wiki/Mike_Tyson").execute
